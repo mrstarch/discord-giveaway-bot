829ce805ba1afdcd9422be48c3c190a27453eed6
Daniel Strauch <64928752+mrstarch@users.noreply.github.com>
2024-01-22 19:34:43 -0800

    Running default app

--- src/utility.js
+++ src/utility.js
@@ -1,15 +1,64 @@
-function convertMillisecondsToTimeFormat(milliseconds) {
-    const seconds = Math.floor(milliseconds / 1000);
-    const days = Math.floor(seconds / 86400);
-    const hours = Math.floor((seconds % 86400) / 3600);
-    const minutes = Math.floor(((seconds % 86400) % 3600) / 60);
-    const remainingSeconds = seconds % 60;
-  
-    const formattedTime = `${days} ${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:${String(remainingSeconds).padStart(2, '0')}`;
-    
-    return formattedTime;
+import 'dotenv/config';
+import fetch from 'node-fetch';
+import verifyKey from 'discord-interactions';
+
+function VerifyDiscordRequest(clientKey) {
+  return function (req, res, buf, encoding) {
+    const signature = req.get('X-Signature-Ed25519');
+    const timestamp = req.get('X-Signature-Timestamp');
+
+    const isValidRequest = verifyKey(buf, signature, timestamp, clientKey);
+    if (!isValidRequest) {
+      res.status(401).send('Bad request signature');
+      throw new Error('Bad request signature');
+    }
+  };
 }
 
-  module.exports = {
-    convertMillisecondsToTimeFormat
+async function DiscordRequest(endpoint, options) {
+  // append endpoint to root API URL
+  const url = 'https://discord.com/api/v10/' + endpoint;
+  // Stringify payloads
+  if (options.body) options.body = JSON.stringify(options.body);
+  // Use node-fetch to make requests
+  const res = await fetch(url, {
+    headers: {
+      Authorization: `Bot ${process.env.DISCORD_TOKEN}`,
+      'Content-Type': 'application/json; charset=UTF-8',
+    },
+    ...options
+  });
+  // throw API errors
+  if (!res.ok) {
+    const data = await res.json();
+    console.log(res.status);
+    throw new Error(JSON.stringify(data));
   }
+  // return original response
+  return res;
+}
+
+async function InstallGuildCommands(appId, commands) {
+  // API endpoint to overwrite global commands
+  const endpoint = `applications/${appId}/guilds/${process.env.GUILD_ID}/commands`;
+
+  try {
+    // This is calling the bulk overwrite endpoint: https://discord.com/developers/docs/interactions/application-commands#bulk-overwrite-global-application-commands
+    await DiscordRequest(endpoint, { method: 'PUT', body: commands });
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+// Simple method that returns a random emoji from list
+function getRandomEmoji() {
+  const emojiList = ['😭','😄','😌','🤓','😎','😤','🤖','😶‍🌫️','🌏','📸','💿','👋','🌊','✨'];
+  return emojiList[Math.floor(Math.random() * emojiList.length)];
+}
+
+export {
+  VerifyDiscordRequest,
+  DiscordRequest,
+  InstallGuildCommands,
+  getRandomEmoji,
+}
